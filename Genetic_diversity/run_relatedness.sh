1981efaceb3670021b2ebdd4dac5eb797c0685c3
stella-huynh <45164634+stella-huynh@users.noreply.github.com>
2020-01-31 05:30:14 +0800

    Create run_relatedness.sh

---
+++ Genetic_diversity/run_relatedness.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SP=$1
+BAMLIST=$2
+REGION=$3
+NIND=$4
+OUTFILE=$5
+NTHREADS=$6
+
+angsd -ref BFAL_genome.fasta \
+      -bam ${BAMLIST} \
+      -rf ${REGION} \
+      -out ${OUTFILE} \
+      -nThreads ${NTHREADS} \
+      -remove_bads 1 -uniqueOnly 1 -only_proper_pairs 0 -minMapQ 20 -minQ 20 -trim 0 \
+      -doMajorMinor 1 -skipTriallelic 1 \
+      -doCounts 1 -doMaf 1 -SNP_pval 1e-6 \
+      -doGeno -32 -doPost 1 \
+      -GL 1 -doGlf 3
+
+zcat ${OUTFILE}.mafs.gz | cut -f6 | sed 1d > ${OUTFILE}.mafs
+
+ngsRelate -g ${OUTFILE}.glf.gz -f ${oDIR}/${OUTFILE}.mafs \
+          -n ${NIND} -O ${oDIR}/${OUTFILE}.related
+
