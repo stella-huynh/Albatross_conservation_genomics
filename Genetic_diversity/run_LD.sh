23857094dac9a46f510a383ca4e402fe527d1b45
stella-huynh <45164634+stella-huynh@users.noreply.github.com>
2020-02-06 13:39:15 +0800

    Update run_LD.sh

--- Genetic_diversity/run_LD.sh
+++ Genetic_diversity/run_LD.sh
@@ -15,8 +15,8 @@ angsd -ref BFAL_genome.fasta \
       -remove_bads 1 -uniqueOnly 1 -only_proper_pairs 0 -minMapQ 20 -minQ 20 -trim 0 \
       -doCounts -1 -doMaf -1 -SNP_pval 1e-6 \
       -doMajorMinor 1 -skipTriallelic 1 \
-      -doGeno 32 -doPost 1 \
-      -GL 1 -doGlf -3
+      -doGeno -32 -doPost 1 -geno_minDepth 1 \
+      -GL 1 -doGlf 3
 
 
 zcat ${PREFIX}.glf.pos.gz > ${PREFIX}.glf.pos
@@ -26,19 +26,19 @@ N_SITES=`cat ${PREFIX}.glf.pos | wc -l`
 
 ngsLD --pos ${PREFIX}.glf.pos \
       --geno ${PREFIX}.glf \
-      --n_ind ${NIND} \
-      --n_sites ${N_SITES} \
       --probs --log_scale \
-      --out ${PREFIX}.ld	# Output files of ~500Gb !
+      --n_ind ${NIND} --n_sites ${N_SITES} \
+      --n_threads ${NTHREADS} \
+      --out ${PREFIX}.ld	# Output files of ~500Gb with default max dist 100kb!
 
 
 ngsLD --pos ${PREFIX}.glf.pos \
       --geno ${PREFIX}.glf \
-      --n_ind ${NIND} \
-      --n_sites ${N_SITES} \
-      --probs --log_scale --extend_out \
+      --probs --log_scale \
+      --n_ind ${NIND} --n_sites ${N_SITES} \
+      --rnd_sample 0.05 --max-kb_dist 10000000 \
       --min_maf 0.05 --call_geno --call_thresh 0.10 --ignore_miss_data \
       --n_threads ${NTHREADS} \
-      --out ${PREFIX}_filt.ld	# Add more filters...
+      --out ${PREFIX}_filt_samp05.ld	# Add more filters...
 
 
