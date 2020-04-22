5d2585d6215c6bd3331d573c98377a0370783208
stella-huynh <45164634+stella-huynh@users.noreply.github.com>
2020-04-22 21:29:19 +0800

    Update Extract_RBH_orthologs.sh

--- phylogeny/Extract_RBH_orthologs.sh
+++ phylogeny/Extract_RBH_orthologs.sh
@@ -53,11 +53,57 @@ awk '{FS=OFS="\t"} NR==FNR {a[$2 FS $3]=$0; next} { idx=$1 FS $2 ; if(idx in a) 
 #Extract and rename orthologs in GFF3 file
 #seqkit grep -r -p $LOC --id-regexp "^(\S\s\S)" tmp.fasta | seqkit replace -p "^.+\s" -r "${SP}_" | head -n1
 
+
+################################################################################
+# Extract corresponding sequence data and separate by orthologous gene (ie. one fasta file per ortholog)
+
 nohup ./run_parse_orthoGenes.sh BFAL 1 gene_set/ > nohup_parseOrtho_BFAL.out 2>&1 &
 nohup ./run_parse_orthoGenes.sh LAAL 2 gene_set/ > nohup_parseOrtho_LAAL.out 2>&1 &
 nohup ./run_parse_orthoGenes.sh STAL 3 gene_set/ > nohup_parseOrtho_STAL.out 2>&1 &
 nohup ./run_parse_orthoGenes.sh WAAL 4 gene_set/ > nohup_parseOrtho_WAAL.out 2>&1 &
 nohup ./run_parse_orthoGenes.sh WAL 5 gene_set/ > nohup_parseOrtho_WAL.out 2>&1 &
+
+#double-check one sequence of each species has effectively been retrieved in the output 
+ #(12619 orthologs retained, should find 5 FASTA headers in each gene file)
+ #for i in {1..12619}; do grep -c "^>" 05_RBlast/orthologs/multiple_hits/gene_set/Blastallp_ALB_ORTH${i}.fasta >> tmp.txt ; done
+ for SP in BFAL LAAL STAL WAAL WAL; do
+ 
+   for i in {1..12619}; do
+	grep -c ">${SP}" 05_RBlast/orthologs/multiple_hits/gene_set/Blastallp_ALB_ORTH${i}.fasta >> tmp.txt
+   done
+   
+   occ=$(uniq tmp.txt) ; echo "${SP} : ${occ}"
+ done
+
+
+################################################################################
+# Locally align sequences per orthologous gene
+
+
+run_pasta.py \
+	--input PATH/to/Blastallp_ALB_ORTH${i}.fasta \
+	--alignment-suffix=.aln \
+	--output-directory=PATH/to/aligned_seq \
+	--datatype=dna \
+	--job PATH/to/aligned_seq/Blastallp_ALB_ORTH${i} \
+	--raxml-search-after \
+	--temporaries=/data/huynhs \
+	--treefile=01_maf/SNAPP/ALB5_start_tree.newick \
+	--num-cpus=${NCORE} \
+	--iter-limit=10 \
+	--aligner=mafft \
+	--merger=muscle \
+	--tree-estimator= &
+
+
+
+
+
+
+
+
+
+
 
 
 
