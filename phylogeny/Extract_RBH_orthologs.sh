fe6500754cf19e3ceffefb570357f20ad196106d
stella-huynh <45164634+stella-huynh@users.noreply.github.com>
2020-04-23 14:15:34 +0800

    Update Extract_RBH_orthologs.sh

--- phylogeny/Extract_RBH_orthologs.sh
+++ phylogeny/Extract_RBH_orthologs.sh
@@ -66,14 +66,25 @@ nohup ./run_parse_orthoGenes.sh WAL 5 gene_set/ > nohup_parseOrtho_WAL.out 2>&1 
 #double-check one sequence of each species has effectively been retrieved in the output 
  #(12619 orthologs retained, should find 5 FASTA headers in each gene file)
  #for i in {1..12619}; do grep -c "^>" 05_RBlast/orthologs/multiple_hits/gene_set/Blastallp_ALB_ORTH${i}.fasta >> tmp.txt ; done
+ col=0
  for SP in BFAL LAAL STAL WAAL WAL; do
  
+   # alt 1 # check all species are present once in each ortholog file
    for i in {1..12619}; do
 	grep -c "^>${SP}" 05_RBlast/orthologs/multiple_hits/gene_set/Blastallp_ALB_ORTH${i}.fasta >> check_orth_${SP}.txt
    done
-   
    occ=$(cat check_orth_${SP}.txt | paste -sd+ | bc) ; echo "${SP} : ${occ} orthologous sequences extracted."
    if [[ $occ != 12619 ]]; then grep -v -n 1 check_orth_${SP}.txt > check_orth_${SP}.err ; fi #retrieve $LOC lines that has failed
+   
+   # alt 2 # check FASTA header of species sequence is as expected from list "Blastallp_ALB.ubesthits.txt"
+   for i in {1..12619}; do
+	grep "^>${SP}" 05_RBlast/orthologs/multiple_hits/gene_set/Blastallp_ALB_ORTH${i}.fasta >> checkseq_orth_${SP}.txt
+   done
+   col=(( col +1 ))
+   checkdiff=$( diff <(cut -f$col Blastallp_ALB.ubesthits.txt) <(sed "s/>${SP}_//g" checkseq_orth_${SP}.txt) )
+   if [[ -z $checkdiff ]]; then echo "No differences observed with list of unique best hits for species : \"${SP}\"."
+   else echo "WARNINGS! : differences are observed with list of unique best hits for species : \"${SP}\"." ; fi
+   
    
  done
 
@@ -81,23 +92,37 @@ nohup ./run_parse_orthoGenes.sh WAL 5 gene_set/ > nohup_parseOrtho_WAL.out 2>&1 
 ################################################################################
 # Locally align sequences per orthologous gene
 
+for i in {1..12619}
+do
+	#create gene folder for local alignment
+	mkdir -p -m775 gene_set/Blastallp_ALB_ORTH$i
+	
+	#reformat starting tree file
+	echo "(WAL_ORTH${i},((BFAL_ORTH${i},LAAL_ORTH${i}),(STAL_ORTH${i},WAAL_ORTH${i})))" > gene_set/Blastallp_ALB_ORTH$i/start_tree${i}.newick
+	
+	#reformat fasta file
+	cp gene_set/Blastallp_ALB_ORTH$i.fasta gene_set/Blastallp_ALB_ORTH$i/.
+	for SP in BFAL LAAL STAL WAAL WAL; do
+		sed -i "s/\(^>${SP}_\).*$/\1ORTH${i}/g" gene_set/Blastallp_ALB_ORTH$i/Blastallp_ALB_ORTH$i.fasta
+	done
+	
+	#run iterative local alignment with PASTA + SATé (using MAFFT as aligner)
+	run_pasta.py \
+		--input gene_set/Blastallp_ALB_ORTH${i}.fasta \
+		--alignment-suffix=.aln \
+		--output-directory=gene_set/Blastallp_ALB_ORTH$i \
+		--datatype=dna \
+		--job gene_set/Blastallp_ALB_ORTH${i} \
+		--raxml-search-after \
+		--temporaries=/data/huynhs \
+		--treefile=gene_set/Blastallp_ALB_ORTH${i}/start_tree${i}.newick \
+		--num-cpus=${NCORE} \
+		--iter-limit=10 \
+		--aligner=mafft \
+		--merger=muscle \
+		--tree-estimator= &
 
-run_pasta.py \
-	--input PATH/to/Blastallp_ALB_ORTH${i}.fasta \
-	--alignment-suffix=.aln \
-	--output-directory=PATH/to/aligned_seq \
-	--datatype=dna \
-	--job PATH/to/aligned_seq/Blastallp_ALB_ORTH${i} \
-	--raxml-search-after \
-	--temporaries=/data/huynhs \
-	--treefile=01_maf/SNAPP/ALB5_start_tree.newick \
-	--num-cpus=${NCORE} \
-	--iter-limit=10 \
-	--aligner=mafft \
-	--merger=muscle \
-	--tree-estimator= &
-
-
+done
 
 
 
