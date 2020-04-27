983efbc6c8dd5c85dc9398a4d804ee4efbc40308
stella-huynh <45164634+stella-huynh@users.noreply.github.com>
2020-04-27 23:58:25 +0800

    Update Extract_RBH_UCEs.sh

--- phylogeny/Extract_RBH_UCEs.sh
+++ phylogeny/Extract_RBH_UCEs.sh
@@ -56,6 +56,27 @@ do
 ???????????????
 
 
+for SP in BFAL LAAL STAL WAAL WAL
+do
+    for OUTG in EURHE FULGL GAVST GALGA
+    do 
+        awk -v SP=$SP '{FS=OFS="\t"} NR==FNR { outg[$2]=$2; sp[$2]=$1; start[$2]=$7; end[$2]=$8; next } ($1 in outg) { i=index(outg[$1],"_"); s=substr(outg[$1],i+1); print SP"_"s, outg[$1], sp[$1], start[$1], end[$1] }' \
+        Megablast_UCEs_${SP}_${OUTG}.besthits.txt \
+        Megablast_UCEs_${OUTG}_${SP}.besthits.txt \
+        > Megablast_UCEs_${SP}_${OUTG}.ubesthits.txt 
+        
+        #checking
+        diff <(cut -f1 Megablast_UCEs_${SP}_${OUTG}.ubesthits.txt | sed "s/^${SP}_//g") \
+             <(cut -f2 Megablast_UCEs_${SP}_${OUTG}.ubesthits.txt | sed "s/${OUTG}_//g")
+    done
+done
+
+
+
+
+
+
+
 ####################################################################
 
 #step 1 : Extract UCEs found in all outgroup for each species
@@ -64,8 +85,8 @@ for SP in BFAL LAAL STAL WAAL WAL;
 do 
     awk '{FS=OFS="\t"} NR==FNR {a[$1 FS $3]=$0; next} {idx=$1 FS $3 ; if(idx in a) print a[idx], $0}' Megablast_UCEs_${SP}_EURHE.ubesthits.txt Megablast_UCEs_${SP}_FULGL.ubesthits.txt \
     | awk '{FS=OFS="\t"} NR==FNR {a[$1 FS $3]=$0; next} {idx=$1 FS $3 ; if(idx in a) print a[idx], $0}' - Megablast_UCEs_${SP}_GAVST.ubesthits.txt \
-    awk '{FS=OFS="\t"} NR==FNR {a[$1 FS $3]=$0; next} {idx=$1 FS $3 ; if(idx in a) print a[idx], $0}' - Megablast_UCEs_${SP}_GALGA.ubesthits.txt \
-    > Megablast_UCEs_${SP}_all.ubesthits.txt    
+    | awk '{FS=OFS="\t"} NR==FNR {a[$1 FS $3]=$0; next} {idx=$1 FS $3 ; if(idx in a) print a[idx], $0}' - Megablast_UCEs_${SP}_GALGA.ubesthits.txt \
+    > Megablast_UCEs_${SP}_all.ubesthits.txt
 done
 
 
@@ -95,12 +116,15 @@ done
 #step 4 : Extract (final) UCEs that are commonly retained in all species
 
 cut -f1 Megablast_UCEs_BFAL_all_overlapEF.ubesthits.txt \
-  | cut -d"_" -f2,3 | \
-  awk '{FS=OFS="\t"}  NR==FNR {scaff[$1]=$0; next} {split($1,a,"_"); idx=a[2]"_"a[3]; if(idx in scaff) print scaff[idx]}' - Megablast_UCEs_LAAL_all_overlapEF.ubesthits.txt \
-  | awk '{FS=OFS="\t"}  NR==FNR {scaff[$1]=$0; next} {split($1,a,"_"); idx=a[2]"_"a[3]; if(idx in scaff) print scaff[idx]}' - Megablast_UCEs_STAL_all_overlapEF.ubesthits.txt \
-  | awk '{FS=OFS="\t"}  NR==FNR {scaff[$1]=$0; next} {split($1,a,"_"); idx=a[2]"_"a[3]; if(idx in scaff) print scaff[idx]}' - Megablast_UCEs_WAAL_all_overlapEF.ubesthits.txt \
-  | awk '{FS=OFS="\t"}  NR==FNR {scaff[$1]=$0; next} {split($1,a,"_"); idx=a[2]"_"a[3]; if(idx in scaff) print scaff[idx]}' - Megablast_UCEs_WAL_all_overlapEF.ubesthits.txt \
+  | cut -d"_" -f2- \
+  | awk '{FS=OFS="\t"}  NR==FNR {scaff[$1]=$0; next} {i=index($1,"_"); idx=substr($1,i+1); if(idx in scaff) print scaff[idx]}' - Megablast_UCEs_LAAL_all_overlapEF.ubesthits.txt \
+  | awk '{FS=OFS="\t"}  NR==FNR {scaff[$1]=$0; next} {i=index($1,"_"); idx=substr($1,i+1); if(idx in scaff) print scaff[idx]}' - Megablast_UCEs_STAL_all_overlapEF.ubesthits.txt \
+  | awk '{FS=OFS="\t"}  NR==FNR {scaff[$1]=$0; next} {i=index($1,"_"); idx=substr($1,i+1); if(idx in scaff) print scaff[idx]}' - Megablast_UCEs_WAAL_all_overlapEF.ubesthits.txt \
+  | awk '{FS=OFS="\t"}  NR==FNR {scaff[$1]=$0; next} {i=index($1,"_"); idx=substr($1,i+1); if(idx in scaff) print scaff[idx]}' - Megablast_UCEs_WAL_all_overlapEF.ubesthits.txt \
   > listF_uniq_UCEs.txt
+
+#command below NOT useful anymore, solution found by replacing split(a,$1,"_") by index+substr in awk command.
+#sed -i '/^chrun_random$/d' listF_uniq_UCEs.txt
 
 
 #step 5 : Retrieve the whole region of these final UCEs for each species
@@ -113,34 +137,43 @@ do
 done 
 
 
+#step 6: extract DNA sequences for each UCEs (one file per gene, all 4 species for each gene)
+
+rDIR="/group/sbs_ssin/stella/albatross/00_genomes/01_WGS/01_dna"
+mkdir -p -m775 gene_set
+
+./run_parse_UCEs.sh -i Megablast_UCEs_EF_BFAL.txtF -f ${rDIR}/BFAL_genome_v2.fasta -o gene_set/ -n yes > nohup_parseUCE_BFAL.out &
+sleep 20
+
+for SP in LAAL STAL WAAL WAL;
+do
+    ./run_parse_UCEs.sh \
+        -i Megablast_UCEs_EF_${SP}.txtF \
+        -f /group/sbs_ssin/stella/albatross/00_genomes/01_WGS/01_dna/${SP}_genome.fasta \
+        -o gene_set/ \
+        -n no \
+        > nohup_parseUCE_${SP}.out &
+    sleep 20
+done
+wait 
+
+  #check all gene files have all species and one sequence per species
+  rm -f check_nUCEs.txt
+  for file in `ls gene_set/*`; do grep -c "^>" $file >> check_nUCEs.txt ; done
+  sort -V check_nUCEs.txt | uniq 
+  
+  for SP in BFAL LAAL STAL WAAL WAL; do
+    rm -f check_UCEs_*.txt
+    for file in `ls gene_set/*`; do
+      grep -c "^>${SP}" $file >> check_UCEs_${SP}.txt 
+    done
+    occ=$(cat check_UCEs_${SP}.txt | paste -sd+ | bc) ; echo "${SP} : ${occ} orthologous sequences extracted."
+    if [[ $occ != $(wc -l listF_uniq_UCEs.txt) ]]; then grep -v -n 1 check_UCEs_${SP}.txt > check_UCEs_${SP}.err ; fi #retrieve UCEs (lines) that has failed
+  done
+  
+  
+  
 
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
