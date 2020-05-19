e1072fe929bb3e842c4c33fcde0f886dd843e063
stella-huynh <45164634+stella-huynh@users.noreply.github.com>
2020-05-19 14:56:34 +0800

    Update 03_MITObim_two-steps.sh

--- Mitochondrial assembly/03_MITObim_two-steps.sh
+++ Mitochondrial assembly/03_MITObim_two-steps.sh
@@ -7,7 +7,27 @@ do
     echo -e "\nStep 1: Initial mapping assembly using MIRA 4.0\n"
     mira manifest_GRCg6a_${SP}.conf >& log_mapping_GRCg6a-${SP}.out &
 
+done
+
+
+
+for SP in BFAL LAAL STAL WAAL WAL
+do
+
     echo -e "\nStep 2: Baiting and iterative mapping using MITObim 1.7\n"
+    
+    if [[ $SP == "BFAL" ]]; then BC="TATCAG"
+    elif [[ $SP == "LAAL" ]]; then BC="GTGAAA"
+    elif [[ $SP == "STAL" ]]; then BC="ATCGTG"
+    elif [[ $SP == "WAAL" ]]; then BC="TAATCG"
+    elif [[ $SP == "WAL" ]]; then BC="GTAGAG"
+    fi
+    
+    interleave-fastqgz-MITOBIM.py \
+            ${SP}_220bp_${BC}.R1.20M.paired.fastq.gz
+            ${SP}_220bp_${BC}.R2.20M.paired.fastq.gz
+            > ${SP}_220bp_${BC}.20M.paired.fastq.gz
+    
     MITObim.pl -start 1 -end 10 --clean \
                -sample ${SP} \
                -ref GRCg6a_genome \
